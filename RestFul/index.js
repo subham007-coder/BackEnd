94f70ec6846d8fc22260a931ee7270f648cee4b7
subham007-coder <dsubham109@gmail.com>
2023-12-07 15:51:46 +0530

    add

--- RestFul/index.js
+++ RestFul/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+app.use(express.urlencoded({extended: true}));
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.set(express.static(path.join(__dirname, "/public")));
+
+
 
 app.get("/", (req, res) => {
     res.send("this is home")
-})
+});
+
 
 
 const port = 8080;
