5e15b2f2023fd0678ee1dbd83fb1c2d40ec28a93
subham007-coder <dsubham109@gmail.com>
2023-12-07 22:33:42 +0530

    add

--- RestFul/index.js
+++ RestFul/index.js
@@ -1,19 +1,38 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-app.use(express.urlencoded({extended: true}));
-
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
-app.set(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "/public")));
+
+let posts = [
+    {
+        username: "Subham Das",
+        content: "With supporting text below as a natural lead-in to additional content."
+    },
+    {
+        username: "Adrija Pal",
+        content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti, commodi!"
+    }
+];
 
 
 
-app.get("/", (req, res) => {
-    res.send("this is home")
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", { posts });
 });
 
+app.get("/posts/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) => {
+    const {username, content} = req.body;
+    posts.push({username, content});
+    res.redirect("/posts");
+});
 
 
 const port = 8080;
