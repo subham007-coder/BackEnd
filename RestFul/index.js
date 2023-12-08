21604bd6e0f399ed72c5ffbf7dc1cdce7285d361
subham007-coder <dsubham109@gmail.com>
2023-12-08 20:32:05 +0530

    add

--- RestFul/index.js
+++ RestFul/index.js
@@ -3,6 +3,7 @@ const { v4: uuidv4 } = require('uuid'); // require korlam uuid ke random id pawa
 
 const app = express();
 const path = require("path");
+const { log } = require("console");
 app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
@@ -34,15 +35,29 @@ app.get("/posts/new", (req, res) => {
 app.post("/posts", (req, res) => {
     const { username, content } = req.body;
     let id = uuidv4();
-    posts.push({id, username, content });
+    posts.push({ id, username, content });
     res.redirect("/posts");
 });
 
 app.get("/posts/:id", (req, res) => {
     let { id } = req.params;
     let post = posts.find((p) => id === p.id);
-    res.render("show.ejs", {post});
+    res.render("show.ejs", { post });
 });
+
+app.get("/posts/edit", (req, res) => {
+    res.render("update.ejs");
+});
+
+app.patch("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+
+    let newContent = req.body.content;
+    post.content = newContent;
+    console.log(post);
+});
+
 
 const port = 8080;
 
